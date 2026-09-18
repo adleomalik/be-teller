7d064c50b192bed682e884c2df0658900c7af511
MALIK\adleo <adleomalik@gmail.com>
2026-09-18 15:25:57 +0700

    cors origin

--- src/main/java/com/adleo/teller/controller/HistoryBankController.java
+++ src/main/java/com/adleo/teller/controller/HistoryBankController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/historyBank")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class HistoryBankController {
     @Autowired
     private HistoryBankService historyBankService;
--- src/main/java/com/adleo/teller/controller/UserController.java
+++ src/main/java/com/adleo/teller/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
